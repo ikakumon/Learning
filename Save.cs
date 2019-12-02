3bcb00ee990e767b22f1c75119c58172bad1b3d8
ikakumon <57615447+ikakumon@users.noreply.github.com>
2019-12-02 22:49:55 +0800

    save the message of the class itself contains

---
+++ Save.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Save
+{
+    public List<int> PositionX = new List<int>();
+    public List<double> PositionY = new List<double>();
+    public List<double> PositionZ = new List<double>();
+    public List<int> Types = new List<int>();
+    public  int SaveNum;
+    //public int[,] blockMap = new int[7, 8]
+    //{
+    //       {1,4,3,1,4,2,3,1 },
+    //       {4,2,4,4,1,2,3,4 },
+    //       {1,2,3,1,1,2,3,1 },
+    //       {4,2,3,1,1,2,3,4 },
+    //       {1,4,3,4,1,2,0,0 },
+    //       {4,2,3,1,4,2,0,1 },
+    //       {1,4,3,4,1,4,4,1 },
+    //   };
+
+
+    public int shootNum = 0;
+    public int score = 0;
+}
